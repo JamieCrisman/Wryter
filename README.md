98701b2ff0c4ad3e4014183d43e89200cdc8e7f7
Jamie Crisman <jamie@awkwardhero.com>
2014-08-18 11:52:58 -0500

    updates... still figuring out saving

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ wryter
 ======
 
 Simple writing chrome app
+
+Do this to compile
+
+``
+vulcanize index.html -o main.html main.js --csp
+``
--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,10 +1,47 @@
 'use strict';
 
-function MainCtrl () {
-  this.title;
-  this.content= "Lorem ipsum dolor sit amet <span style='color:red;'>a</span><span style='color:blue;'>b</span><span style='color:brown;'>c</span>";
+function FileSys($scope){
+  var ret = {};
+  ret.openFileSystem = function(){
+    console.log("opening file system start");
+    if (!chrome || !chrome.syncFileSystem || !chrome.syncFileSystem.requestFileSystem) {
+      console.error('Syncable FileSystem is not supported in your environment.');
+      return;
+    }
+    console.log("Syncable File System supported");
+    if (chrome.syncFileSystem.setConflictResolutionPolicy) {
+      chrome.syncFileSystem.setConflictResolutionPolicy('last_write_win');
+    }
+    console.log("conflict resolution policy set");
+    console.log('Obtaining syncable FileSystem...');
+    chrome.syncFileSystem.requestFileSystem(function (fs) {
+      if (chrome.runtime.lastError) {
+        console.error('requestFileSystem: ' + chrome.runtime.lastError.message);
+        return;
+      }
+      $scope.$emit("fileSystemLoaded", fs);
+    });
+    
+  };
+  
+  return ret;
 }
 
 angular
   .module('Wryter', ['angular-medium-editor'])
-  .controller('MainController', MainCtrl);
+  .controller('MainController', function($scope){
+    this.title;
+    this.content= "Lorem ipsum dolor sit amet <span style='color:red;'>a</span><span style='color:blue;'>b</span><span style='color:brown;'>c</span>";
+    this.supportSFS = chrome && chrome.syncFileSystem;
+    this.hasLoaded = false;
+    this.fileSystem = null;
+    FileSys($scope).openFileSystem();
+    $scope.$on("fileSystemLoaded", function(e, file){
+      $scope.main.hasLoaded = true;
+      $scope.$apply(function(){
+        $scope.main.fileSystem = file;
+        console.log($scope.main.fileSystem);
+      });
+    });
+  })
+  .factory('FileSys', FileSys);
